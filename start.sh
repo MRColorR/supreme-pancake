2de936ed70a2deb1d2a98adf8f4048a612882894
Mattia <bigdata@bigdata-VirtualBox>
2021-12-12 08:21:27 +0100

    updatet sleep times in start.sh

--- start.sh
+++ start.sh
@@ -9,7 +9,7 @@ read continue
 echo "Starting Hadoop cluster"
 gnome-terminal -- sh $HADOOP_DIR/start-hadoop.sh & echo HadoopCluster:Starting
 
-sleep 5
+sleep 10
 
 echo "Starting zookeper"
 gnome-terminal -- sh kafka_2.13-3.0.0/bin/zookeeper-server-start.sh kafka_2.13-3.0.0/config/zookeeper.properties & echo zookeper-server:Starting
@@ -30,9 +30,9 @@ sleep 5
 echo "Starting Cassandra..."
 gnome-terminal -- service cassandra start & echo Cassandra:Starting
 
-sleep 5
+sleep 7
 
 echo "Setting-up Cassandra keyspace and tables..."
 gnome-terminal -- cqlsh mattia -f ./cassandra-db/dbscript.cql & echo Done
-sleep 5
+sleep 12
 echo "Project starting sequence complete"
