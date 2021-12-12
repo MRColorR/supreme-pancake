db3d44e4eaa3d406756c0676f8570b5d7b0f6575
Mattia <bigdata@bigdata-VirtualBox>
2021-12-12 08:26:00 +0100

    increased sleep for cassandra service

--- start.sh
+++ start.sh
@@ -9,7 +9,7 @@ read continue
 echo "Starting Hadoop cluster"
 gnome-terminal -- sh $HADOOP_DIR/start-hadoop.sh & echo HadoopCluster:Starting
 
-sleep 10
+sleep 12
 
 echo "Starting zookeper"
 gnome-terminal -- sh kafka_2.13-3.0.0/bin/zookeeper-server-start.sh kafka_2.13-3.0.0/config/zookeeper.properties & echo zookeper-server:Starting
@@ -30,7 +30,7 @@ sleep 5
 echo "Starting Cassandra..."
 gnome-terminal -- service cassandra start & echo Cassandra:Starting
 
-sleep 7
+sleep 10
 
 echo "Setting-up Cassandra keyspace and tables..."
 gnome-terminal -- cqlsh mattia -f ./cassandra-db/dbscript.cql & echo working...
